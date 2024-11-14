d3f8cb92c9434da6eeb0f3cb79032344d249ead1
peterberekvolgyi <berekvolgyipeter@gmail.com>
2024-11-14 20:02:24 +0100

    base airdrop contract

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,15 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
+import { MerkleProof } from "openzeppelin/utils/cryptography/MerkleProof.sol";
+import { IERC20, SafeERC20 } from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
+
+/**
+ * @title Merkle Airdrop - Airdrop tokens to users who can prove they are in a merkle tree
+ */
 contract MerkleAirdrop {
-    // list of addresses that can receive tokens
-    address[] claimers;
+    using SafeERC20 for IERC20; // Prevent sending tokens to recipients who can’t receive
+
+    error MerkleAirdrop__InvalidProof();
+
+    IERC20 private immutable i_airdropToken;
+    bytes32 private immutable i_merkleRoot;
+
+    event Claim(address indexed account, uint256 indexed amount);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
 
     // allow someone in the list to claim some tokens
-    function claim(address account) external {
-        for (uint256 i = 0; i < claimers.length; i++) {
-            // check if the account is in the claimers array
-            // but this method is gonna be costly and prone to DoS
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
         }
+        i_airdropToken.safeTransfer(account, amount);
     }
 }
