833c56541227de40b01665bfbe200e0ace8c604d
peterberekvolgyi <berekvolgyipeter@gmail.com>
2024-11-14 20:18:09 +0100

    already claimed check

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -11,9 +11,11 @@ contract MerkleAirdrop {
     using SafeERC20 for IERC20; // Prevent sending tokens to recipients who can’t receive
 
     error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
 
     IERC20 private immutable i_airdropToken;
     bytes32 private immutable i_merkleRoot;
+    mapping(address => bool) private s_hasClaimed;
 
     event Claim(address indexed account, uint256 indexed amount);
 
@@ -24,10 +26,21 @@ contract MerkleAirdrop {
 
     // allow someone in the list to claim some tokens
     function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        if (s_hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+
+        // If an attacker were to find two different inputs that produce the same hash (a collision)
+        // with a single hash function, they could potentially manipulate data in a Merkle tree.
+        // Double hashing makes it significantly harder to find two inputs that collide at the leaf level,
+        // as it requires collisions for two different rounds of hashing.
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
         if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
             revert MerkleAirdrop__InvalidProof();
         }
+
+        s_hasClaimed[account] = true; // prevent users claiming more than once and draining the contract
+        emit Claim(account, amount);
         i_airdropToken.safeTransfer(account, amount);
     }
 }
