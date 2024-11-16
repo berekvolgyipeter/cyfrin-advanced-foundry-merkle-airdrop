c4ccd7df93c93a6148487fe39e58e8016e1c9ef9
peterberekvolgyi <berekvolgyipeter@gmail.com>
2024-11-16 17:23:59 +0100

    implement unit test

--- test/unit/MerkleAirdropTest.t.sol
+++ test/unit/MerkleAirdropTest.t.sol
@@ -2,5 +2,43 @@
 pragma solidity ^0.8.27;
 
 import { Test } from "forge-std/Test.sol";
+import { console } from "forge-std/console.sol";
+import { MerkleAirdrop } from "src/MerkleAirdrop.sol";
+import { BagelToken } from "src/BagelToken.sol";
 
-contract MerkleAirdropTest is Test { }
+contract MerkleAirdropTest is Test {
+    MerkleAirdrop airdrop;
+    BagelToken token;
+    address user;
+    uint256 userPrivKey;
+
+    bytes32 merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 amountToCollect = 25 ether;
+    uint256 amountToSend = amountToCollect * 4; // 4 addresses are whitelisted
+
+    // proofs of the first input address in the generated merkle tree
+    bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] proof = [proofOne, proofTwo];
+
+    function setUp() public {
+        token = new BagelToken();
+        airdrop = new MerkleAirdrop(merkleRoot, token);
+        token.mint(token.owner(), amountToSend);
+        token.transfer(address(airdrop), amountToSend);
+
+        // this is used as the first input address of the merkle tree generation in scripts
+        (user, userPrivKey) = makeAddrAndKey("user");
+    }
+
+    function testUsersCanClaim() public {
+        uint256 startingBalance = token.balanceOf(user);
+
+        vm.prank(user);
+        airdrop.claim(user, amountToCollect, proof);
+
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("Ending balance: %d", endingBalance);
+        assertEq(endingBalance - startingBalance, amountToCollect);
+    }
+}
